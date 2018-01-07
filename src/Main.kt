8c3ae0e13873e3063606e8367af6b3a548e3408a
Samer <samer_n91@hotmail.com>
2018-01-07 21:42:31 +0100

    first

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,26 @@
+import java.io.*
+import java.util.*
 /**
  * Created by Samer on 7/1/2018.
  */
 
-
-import java.util.*
-
-
 fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
-    val num1 = sc.nextInt()
-    val num2 = sc.nextInt()
+    val arraySize = sc.nextInt()
+    val opSize = sc.nextInt()
+
+    val array = LongArray(arraySize)
+
+    (1..opSize).forEach {
+        val from = sc.nextInt() - 1
+        val to = sc.nextInt() - 1
+        val amout = sc.nextInt()
+
+        (from..to).forEach {
+            array[it] = array[it] + amout
+        }
+
+    }
+    println(array.max())
+
 }
