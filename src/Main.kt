cd6f594e18e117246e3b11503b334ec9d8dacc30
Samer <samer_n91@hotmail.com>
2018-01-07 18:08:32 +0100

    INIT

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Samer on 7/1/2018.
+ */
+
+
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+}
