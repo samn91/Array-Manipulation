f12d23d3f9f1f94ab7168e1e7fadf69868e16c27
Samer <samer_n91@hotmail.com>
2018-01-10 22:51:13 +0100

    final soluation

--- src/Main.kt
+++ src/Main.kt
@@ -4,58 +4,27 @@
 
 import java.io.*
 import java.util.*
-import kotlin.system.measureNanoTime
-
-data class Ops(
-        val start: Int, val end: Int, val amount: Long)
 
 
 fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`)
-    val arraySize = sc.nextInt()
-    val opSize = sc.nextInt()
-
-    val res = LongArray(arraySize)
-    val input = Array(arraySize, { Ops(0, 0, 0) })
-
-    for (it in 1..opSize) {
-        val from = sc.nextInt() - 1
-        val to = sc.nextInt() - 1
-        val rawAmount = sc.nextLong()
-
-        input[it - 1] = Ops(from, to, rawAmount)
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val m = scanner.nextInt()
+    val output = LongArray(n)
+    (1..m).forEach {
+        val a = scanner.nextInt() - 1
+        val b = scanner.nextInt()
+        val k = scanner.nextInt()
+        output[a] += k.toLong()
+        if (b < n) output[b] -= k.toLong()
     }
-
-    val currentTimeMillis = System.currentTimeMillis()
-//    input.sortBy { it.start }
-
-    val sortedInput = input.sortedWith(compareBy({ it.start }, { it.end }))
-
+    var sum = 0L
     var max = 0L
-    var holder: Ops? = null
-    for (i in 1 until sortedInput.size) {
-        val ops = sortedInput[i]
-        val prev = sortedInput[i - 1]
-        holder?.let {
-            if (it.start < ops.start) {
-                holder = null
-            } else {
-                max = maxOf(it.amount, ops.amount, max)
-            }
-        }
-
-        if (prev.end < ops.start) {
-            max = maxOf(prev.amount, ops.amount, max)
-        } else {
-            holder = Ops(ops.start, prev.end, ops.amount + prev.amount)
-            max = maxOf(holder!!.amount, max)
-        }
-
-
+    for (i in 0 until n) {
+        sum += output[i]
+        if (sum > max) max = sum
     }
-    println(System.currentTimeMillis() - currentTimeMillis)
-
-
-
+    println(max)
+    scanner.close()
 }
 
